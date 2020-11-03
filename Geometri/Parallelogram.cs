b6843ba84c23a0575f1b6aab94f006822696bb38
staselen <mads464@yahoo.com>
2020-11-03 12:57:29 +0100

    Add files via upload

---
+++ Geometri/Parallelogram.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometriii
+{
+    class Parallelogram : Square
+    {
+        public double b { get; set; }
+
+        public Parallelogram(int A, int B) : base(A)
+        {
+            name = "parallelogram";
+            a = A;
+            b = B;
+        }
+
+        readonly double sinAngle = Math.Sin(45);
+
+
+        public override double CalculateCircumference()
+        {
+            double circumference = (a + b) * 2;
+
+            return circumference;
+
+        }
+
+        public override double CalculateArea()
+        {
+            double area = a * b * sinAngle;
+
+            return area;
+        }
+
+    }
+}
---
+++ Geometri/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometriii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square(10);
+            Square rectangle = new Rectangle(10, 5);
+            Square parallelogram = new Parallelogram(10, 5);
+            Square trapozoid = new Trapezoid(16, 8, 8, 8);
+
+            Console.WriteLine(square.ToString());
+            Console.WriteLine(rectangle.ToString());
+            Console.WriteLine(parallelogram.ToString());
+            Console.WriteLine(trapozoid.ToString());
+           
+            Console.ReadKey();
+        }
+    }
+}
