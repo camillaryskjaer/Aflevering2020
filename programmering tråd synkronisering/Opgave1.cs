aecb7126fc3fffc731f721bd3be460614041f46f
staselen <mads464@yahoo.com>
2020-12-15 12:05:06 +0100

    Add files via upload

---
+++ programmering tråd synkronisering/Opgave1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Tråd2
+{
+    class Program
+    {
+        static int count = 0;
+        static object x = new object();
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(CountPlusPlus);
+            Thread t2 = new Thread(CountMinus);
+
+            
+            t1.Start();
+            t2.Start();
+        }
+
+        static void CountPlusPlus(object arg)
+        {
+            while (true)
+            {
+                Monitor.Enter(x);
+                try
+                {
+                    count++;
+                    Console.WriteLine("ThreadID:{0} | + + + | Total:{1}", Thread.CurrentThread.ManagedThreadId, count);
+                    Thread.Sleep(500);
+                    count++;
+                    Console.WriteLine("ThreadID:{0} | + + + | Total:{1}", Thread.CurrentThread.ManagedThreadId, count);
+                    Thread.Sleep(500);
+
+                }
+                finally
+                {
+                    Monitor.Exit(x);
+                }
+
+            }
+        }
+
+        static void CountMinus(object arg)
+        {
+            while (true)
+            {
+                Monitor.Enter(x);
+                try
+                {
+                    count--;
+                    Console.WriteLine("ThreadID:{0} | - - - | Total:{1}", Thread.CurrentThread.ManagedThreadId, count);
+                    Thread.Sleep(1000);
+                }
+                finally
+                {
+                    Monitor.Exit(x);
+                }
+
+            }
+        }
+    }
+}
---
+++ programmering tråd synkronisering/Opgave2og3.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Tråd2
+{
+    class Program
+    {
+        static object x = new object();
+        static void Main(string[] args)
+        {
+
+            Console.SetWindowSize(60, 20);
+
+            Thread t1 = new Thread(Star);
+            Thread t2 = new Thread(Fence);
+            t1.Start();
+            t2.Start();
+        }
+
+        static void Star(object arg)
+        {
+            while (true)
+            {
+                Monitor.Enter(x);
+                try
+                {
+                    for (int i = 0; i < 60; i++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+
+                }
+                finally
+                {
+                    Monitor.Exit(x);
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        static void Fence(object arg)
+        {
+            while (true)
+            {
+                Monitor.Enter(x);
+                try
+                {
+                    for (int i = 0; i < 60; i++)
+                    {
+                        Console.Write("#");
+                    }
+                    Console.WriteLine();
+
+                }
+                finally
+                {
+                    Monitor.Exit(x);
+                    Thread.Sleep(100);
+                }
+            }
+        }
+    }
+}
